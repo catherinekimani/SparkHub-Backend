385bb14d377579f3a8cfa8229f6b32343e4dcc04
catherinekimani <catherinekimani882@gmail.com>
2024-03-21 21:31:28 +0300

    Remove index view

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
-def index(request):
-    return JsonResponse({'message': 'Welcome to SparkHub!'})
